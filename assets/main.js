acd665af34b50f53efbee03ebdbfbc4f123cf769
De Silva Jayasinghe, Kevin <k2desilvajayasinghe@ucsd.edu>
2020-10-29 13:52:26 -0700

    addition of custom barcode screen

---
+++ assets/main.js
@@ -0,0 +1,49 @@
+document.getElementById("randombarcode").onclick = function () {
+    location.href = "rand_barcode.html";
+};
+
+document.getElementById("custombarcode").onclick = function () {
+    location.href = "custom_barcode.html";
+};
+
+document.getElementById("random2d").onclick = function () {
+    location.href = "rand_datamatrix.html";
+};
+
+document.getElementById("custom2d").onclick = function () {
+    location.href = "custom_datamatrix.html";
+};
+
+function generateRandomBarcode() {
+    var generatedCode = "EXC_TEST_" + createRandomCode(7);
+    var newBarcode = JsBarcode("#barcode", generatedCode, {
+        format: "CODE128",
+        linecolor: "0000",
+        fontSize: 50,
+        width: 4,
+        height: 200,
+    })
+    return newBarcode;
+}
+
+function generateCustomBarcode() {
+    var generatedCode = "EXC_TEST_" + document.getElementById('customstring').value;
+    var newBarcode = JsBarcode("#barcode", generatedCode, {
+        format: "CODE128",
+        linecolor: "0000",
+        fontSize: 50,
+        width: 4,
+        height: 200,
+    })
+    return newBarcode;
+}
+
+function createRandomCode(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
